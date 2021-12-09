fd14d14305bdc1f07e92e270a0988a5cf262a8ac
Théodore Cadet <cadetthe@gmail.com>
2021-12-09 12:53:31 +0000

    front single

--- dorset-team-4-ticketing/src/app/landing-page/landing-page.component.ts
+++ dorset-team-4-ticketing/src/app/landing-page/landing-page.component.ts
@@ -14,17 +14,16 @@ export class LandingPageComponent implements OnInit {
   newMovies: any;
   trendingMovies: any;
   familyMovies: any;
-  
+
   myArray: any[] = [];
   newArrayany: any[] = [];
 
   ngOnInit() {
-    this.newMovies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    this.trendingMovies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    this.familyMovies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // this.newMovies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // this.trendingMovies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // this.familyMovies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     // this.getAll();
-    this.newArrayany = [0, 2, 3]
-
+    // this.newArrayany = [0, 2, 3]
 
     this.store
       .collection("movies")
@@ -42,4 +41,47 @@ export class LandingPageComponent implements OnInit {
     // console.log(this.newArrayany);
   }
 
+  // createArray() {
+  //   var n = 0;
+  //   var m = 0;
+
+  //   var x = 0;
+  //   var lol = [];
+  //   var alphaB = [
+  //     "A",
+  //     "B",
+  //     "C",
+  //     "D",
+  //     "E",
+  //     "F",
+  //     "G",
+  //     "H",
+  //     "I",
+  //     "J",
+  //   ];
+
+  //   while (n <= 99) {
+
+
+  //     var mdr = {
+  //       horizontal: (m + 1).toString(),
+  //       vertical: alphaB[x],
+  //       isTaken: false,
+  //     }
+  //     n++;
+  //     m++;
+  //     lol.push(mdr);
+  //     console.log(n);
+
+  //     if (n % 10 == 0) {
+  //       x++;
+  //       m = 0;
+
+  //     }
+
+
+  //   }
+  //   console.log(lol);
+  // }
 }
+
