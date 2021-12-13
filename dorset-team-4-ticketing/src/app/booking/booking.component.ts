4e8efef4cda054ce09d40daf48698f58f91bb2a3
Théodore Cadet <cadetthe@gmail.com>
2021-12-13 18:06:13 +0000

    selecting seat in the db

--- dorset-team-4-ticketing/src/app/booking/booking.component.ts
+++ dorset-team-4-ticketing/src/app/booking/booking.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from "../data.service";
+import { DataService } from "../data.service";
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-booking',
@@ -7,14 +11,88 @@ import {DataService} from "../data.service";
   styleUrls: ['./booking.component.scss'],
 })
 export class BookingComponent implements OnInit {
+  router: any;
 
-  constructor(public data: DataService) { }
+  private sub: any;
 
-  ngOnInit() {this.toggleSeats()}
+  constructor(public data: DataService, private route: ActivatedRoute, private store: AngularFirestore) { }
 
-  toggleSeats() : void {
+  movieSeats: any;
+
+
+  ngOnInit() {
+    this.toggleSeats();
+    // this.findSeatsInDb();
+  }
+
+  toggleSeats(): void {
     let component = $("#select-seat");
     this.data.booking.adults > 0 ? component.show() : component.hide();
+  }
+
+  findSeatsInDb(seats) {
+
+
+
+    this.store
+      .collection("movies")
+      .doc("FourthMovie")
+      .get()
+      .subscribe((ss) => {
+
+
+        this.movieSeats = ss.data()
+        this.movieSeats = this.movieSeats.seats;
+
+        seats.forEach(e => {
+          let column = e.n.toString();
+          let row = e.l;
+
+          let n = 0;
+          let seatExist = false;
+          console.log(" this.movieSeats.length" + this.movieSeats.length);
+          if (this.movieSeats[n].horizontal == column && this.movieSeats[n].vertical == row) {
+            seatExist = true;
+            this.movieSeats[n].isTaken = true;
+            console.log(1);
+
+
+
+          } else {
+            while (n < this.movieSeats.length && this.movieSeats[n].horizontal != column || this.movieSeats[n].vertical != row) {
+              n++;
+
+              if (this.movieSeats[n].horizontal == column && this.movieSeats[n].vertical == row) {
+                seatExist = true;
+                this.movieSeats[n].isTaken = true;
+
+              }
+
+            }
+
+          }
+
+          if (seatExist) {
+
+
+          }
+        })
+        this.updateSelectedSeats(this.movieSeats);
+
+      });
+
+
+
+
+  }
+
+  updateSelectedSeats(n) {
+    this.store
+      .collection("movies")
+      .doc("FourthMovie")
+      .update({
+        seats: n
+      })
   }
 
 }
--- dorset-team-4-ticketing/src/app/single-movie-page/single-movie-page.component.ts
+++ dorset-team-4-ticketing/src/app/single-movie-page/single-movie-page.component.ts
@@ -19,6 +19,508 @@ export class SingleMoviePageComponent implements OnInit {
   theMovie: any[] = [];
   name: any;
   theMovieInfo: any;
+  theTab: any[] = [
+    {
+      "horizontal": "1",
+      "vertical": "A",
+      "isTaken": false
+    },
+    {
+      "horizontal": "2",
+      "vertical": "A",
+      "isTaken": false
+    },
+    {
+      "horizontal": "3",
+      "vertical": "A",
+      "isTaken": false
+    },
+    {
+      "horizontal": "4",
+      "vertical": "A",
+      "isTaken": false
+    },
+    {
+      "horizontal": "5",
+      "vertical": "A",
+      "isTaken": false
+    },
+    {
+      "horizontal": "6",
+      "vertical": "A",
+      "isTaken": false
+    },
+    {
+      "horizontal": "7",
+      "vertical": "A",
+      "isTaken": false
+    },
+    {
+      "horizontal": "8",
+      "vertical": "A",
+      "isTaken": false
+    },
+    {
+      "horizontal": "9",
+      "vertical": "A",
+      "isTaken": false
+    },
+    {
+      "horizontal": "10",
+      "vertical": "A",
+      "isTaken": false
+    },
+    {
+      "horizontal": "1",
+      "vertical": "B",
+      "isTaken": false
+    },
+    {
+      "horizontal": "2",
+      "vertical": "B",
+      "isTaken": false
+    },
+    {
+      "horizontal": "3",
+      "vertical": "B",
+      "isTaken": false
+    },
+    {
+      "horizontal": "4",
+      "vertical": "B",
+      "isTaken": false
+    },
+    {
+      "horizontal": "5",
+      "vertical": "B",
+      "isTaken": false
+    },
+    {
+      "horizontal": "6",
+      "vertical": "B",
+      "isTaken": false
+    },
+    {
+      "horizontal": "7",
+      "vertical": "B",
+      "isTaken": false
+    },
+    {
+      "horizontal": "8",
+      "vertical": "B",
+      "isTaken": false
+    },
+    {
+      "horizontal": "9",
+      "vertical": "B",
+      "isTaken": false
+    },
+    {
+      "horizontal": "10",
+      "vertical": "B",
+      "isTaken": false
+    },
+    {
+      "horizontal": "1",
+      "vertical": "C",
+      "isTaken": false
+    },
+    {
+      "horizontal": "2",
+      "vertical": "C",
+      "isTaken": false
+    },
+    {
+      "horizontal": "3",
+      "vertical": "C",
+      "isTaken": false
+    },
+    {
+      "horizontal": "4",
+      "vertical": "C",
+      "isTaken": false
+    },
+    {
+      "horizontal": "5",
+      "vertical": "C",
+      "isTaken": false
+    },
+    {
+      "horizontal": "6",
+      "vertical": "C",
+      "isTaken": false
+    },
+    {
+      "horizontal": "7",
+      "vertical": "C",
+      "isTaken": false
+    },
+    {
+      "horizontal": "8",
+      "vertical": "C",
+      "isTaken": false
+    },
+    {
+      "horizontal": "9",
+      "vertical": "C",
+      "isTaken": false
+    },
+    {
+      "horizontal": "10",
+      "vertical": "C",
+      "isTaken": false
+    },
+    {
+      "horizontal": "1",
+      "vertical": "D",
+      "isTaken": false
+    },
+    {
+      "horizontal": "2",
+      "vertical": "D",
+      "isTaken": false
+    },
+    {
+      "horizontal": "3",
+      "vertical": "D",
+      "isTaken": false
+    },
+    {
+      "horizontal": "4",
+      "vertical": "D",
+      "isTaken": false
+    },
+    {
+      "horizontal": "5",
+      "vertical": "D",
+      "isTaken": false
+    },
+    {
+      "horizontal": "6",
+      "vertical": "D",
+      "isTaken": false
+    },
+    {
+      "horizontal": "7",
+      "vertical": "D",
+      "isTaken": false
+    },
+    {
+      "horizontal": "8",
+      "vertical": "D",
+      "isTaken": false
+    },
+    {
+      "horizontal": "9",
+      "vertical": "D",
+      "isTaken": false
+    },
+    {
+      "horizontal": "10",
+      "vertical": "D",
+      "isTaken": false
+    },
+    {
+      "horizontal": "1",
+      "vertical": "E",
+      "isTaken": false
+    },
+    {
+      "horizontal": "2",
+      "vertical": "E",
+      "isTaken": false
+    },
+    {
+      "horizontal": "3",
+      "vertical": "E",
+      "isTaken": false
+    },
+    {
+      "horizontal": "4",
+      "vertical": "E",
+      "isTaken": false
+    },
+    {
+      "horizontal": "5",
+      "vertical": "E",
+      "isTaken": false
+    },
+    {
+      "horizontal": "6",
+      "vertical": "E",
+      "isTaken": false
+    },
+    {
+      "horizontal": "7",
+      "vertical": "E",
+      "isTaken": false
+    },
+    {
+      "horizontal": "8",
+      "vertical": "E",
+      "isTaken": false
+    },
+    {
+      "horizontal": "9",
+      "vertical": "E",
+      "isTaken": false
+    },
+    {
+      "horizontal": "10",
+      "vertical": "E",
+      "isTaken": false
+    },
+    {
+      "horizontal": "1",
+      "vertical": "F",
+      "isTaken": false
+    },
+    {
+      "horizontal": "2",
+      "vertical": "F",
+      "isTaken": false
+    },
+    {
+      "horizontal": "3",
+      "vertical": "F",
+      "isTaken": false
+    },
+    {
+      "horizontal": "4",
+      "vertical": "F",
+      "isTaken": false
+    },
+    {
+      "horizontal": "5",
+      "vertical": "F",
+      "isTaken": false
+    },
+    {
+      "horizontal": "6",
+      "vertical": "F",
+      "isTaken": false
+    },
+    {
+      "horizontal": "7",
+      "vertical": "F",
+      "isTaken": false
+    },
+    {
+      "horizontal": "8",
+      "vertical": "F",
+      "isTaken": false
+    },
+    {
+      "horizontal": "9",
+      "vertical": "F",
+      "isTaken": false
+    },
+    {
+      "horizontal": "10",
+      "vertical": "F",
+      "isTaken": false
+    },
+    {
+      "horizontal": "1",
+      "vertical": "G",
+      "isTaken": false
+    },
+    {
+      "horizontal": "2",
+      "vertical": "G",
+      "isTaken": false
+    },
+    {
+      "horizontal": "3",
+      "vertical": "G",
+      "isTaken": false
+    },
+    {
+      "horizontal": "4",
+      "vertical": "G",
+      "isTaken": false
+    },
+    {
+      "horizontal": "5",
+      "vertical": "G",
+      "isTaken": false
+    },
+    {
+      "horizontal": "6",
+      "vertical": "G",
+      "isTaken": false
+    },
+    {
+      "horizontal": "7",
+      "vertical": "G",
+      "isTaken": false
+    },
+    {
+      "horizontal": "8",
+      "vertical": "G",
+      "isTaken": false
+    },
+    {
+      "horizontal": "9",
+      "vertical": "G",
+      "isTaken": false
+    },
+    {
+      "horizontal": "10",
+      "vertical": "G",
+      "isTaken": false
+    },
+    {
+      "horizontal": "1",
+      "vertical": "H",
+      "isTaken": false
+    },
+    {
+      "horizontal": "2",
+      "vertical": "H",
+      "isTaken": false
+    },
+    {
+      "horizontal": "3",
+      "vertical": "H",
+      "isTaken": false
+    },
+    {
+      "horizontal": "4",
+      "vertical": "H",
+      "isTaken": false
+    },
+    {
+      "horizontal": "5",
+      "vertical": "H",
+      "isTaken": false
+    },
+    {
+      "horizontal": "6",
+      "vertical": "H",
+      "isTaken": false
+    },
+    {
+      "horizontal": "7",
+      "vertical": "H",
+      "isTaken": false
+    },
+    {
+      "horizontal": "8",
+      "vertical": "H",
+      "isTaken": false
+    },
+    {
+      "horizontal": "9",
+      "vertical": "H",
+      "isTaken": false
+    },
+    {
+      "horizontal": "10",
+      "vertical": "H",
+      "isTaken": false
+    },
+    {
+      "horizontal": "1",
+      "vertical": "I",
+      "isTaken": false
+    },
+    {
+      "horizontal": "2",
+      "vertical": "I",
+      "isTaken": false
+    },
+    {
+      "horizontal": "3",
+      "vertical": "I",
+      "isTaken": false
+    },
+    {
+      "horizontal": "4",
+      "vertical": "I",
+      "isTaken": false
+    },
+    {
+      "horizontal": "5",
+      "vertical": "I",
+      "isTaken": false
+    },
+    {
+      "horizontal": "6",
+      "vertical": "I",
+      "isTaken": false
+    },
+    {
+      "horizontal": "7",
+      "vertical": "I",
+      "isTaken": false
+    },
+    {
+      "horizontal": "8",
+      "vertical": "I",
+      "isTaken": false
+    },
+    {
+      "horizontal": "9",
+      "vertical": "I",
+      "isTaken": false
+    },
+    {
+      "horizontal": "10",
+      "vertical": "I",
+      "isTaken": false
+    },
+    {
+      "horizontal": "1",
+      "vertical": "J",
+      "isTaken": false
+    },
+    {
+      "horizontal": "2",
+      "vertical": "J",
+      "isTaken": false
+    },
+    {
+      "horizontal": "3",
+      "vertical": "J",
+      "isTaken": false
+    },
+    {
+      "horizontal": "4",
+      "vertical": "J",
+      "isTaken": false
+    },
+    {
+      "horizontal": "5",
+      "vertical": "J",
+      "isTaken": false
+    },
+    {
+      "horizontal": "6",
+      "vertical": "J",
+      "isTaken": false
+    },
+    {
+      "horizontal": "7",
+      "vertical": "J",
+      "isTaken": false
+    },
+    {
+      "horizontal": "8",
+      "vertical": "J",
+      "isTaken": false
+    },
+    {
+      "horizontal": "9",
+      "vertical": "J",
+      "isTaken": false
+    },
+    {
+      "horizontal": "10",
+      "vertical": "J",
+      "isTaken": false
+    }
+  ];
 
 
   ngOnInit() {
@@ -39,17 +541,19 @@ export class SingleMoviePageComponent implements OnInit {
         });
 
         this.findTheMovie(this.name);
-        // console.log(this.theMovieInfo);
 
       });
 
-      this.store
-      .collection("movies")
-
+    this.updateMovies(this.theTab)
   }
 
-  theUltimateTest(){
-
+  updateMovies(n) {
+    this.store
+      .collection("movies")
+      .doc("FourthMovie")
+      .update({
+        seats: n
+      })
   }
 
   findTheMovie(nameOfMovie) {
